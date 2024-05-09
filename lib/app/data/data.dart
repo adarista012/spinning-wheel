b59a2d83a04666c7a077d53795dc24fc3ae68261
adarista012 <ad.arista012@gmail.com>
2024-05-09 00:01:12 -0400

    data

--- lib/app/data/data.dart
+++ lib/app/data/data.dart
@@ -12,4 +12,21 @@ class Data {
     Colors.red: (3 / 2) * pi,
     Colors.white: (7 / 4) * pi,
   };
+  static Map<String, String> meaningsMap = {
+    'Orange': 'Is used to heal the lungs and to increase energy levels.',
+    'Brown':
+        'Because brown is the color of the earth, it is often used to symbolize nature, autumn, melancholy, or death.',
+    'Gray':
+        'The experts suggest that people who drive gray cars dont want to stand out and instead prefer something more subtle.',
+    'Pink':
+        'Color psychology suggests that different colors can have an impact on our moods, feelings, and even behaviors. The color pink, for example, is thought to be a calming color associated with love, kindness, and femininity.',
+    'Green':
+        'Research in color psychology suggests that colors evoke psychological reactions, affecting moods and emotions.Sometimes, these reactions are related to the intensity of a color. In other cases, they are products of experience and cultural influences.',
+    'Yellow':
+        'The color yellow can be bright and intense, which is perhaps why it can often invoke such strong feelings. Yellow can quickly grab attention, but it can also be abrasive when overused. It can appear warm and bright, yet it can also lead to visual fatigue.',
+    'White':
+        'As many of our readers have suggested, the color white can feel fresh and clean. The color is often used to evoke a sense of youth and modernity.',
+    'Red':
+        'Red is a bold, attention-getting color, so preferring this type of car might mean you want to project an image of power, action, and confidence.',
+  };
 }
--- lib/app/home/home_controller.dart
+++ lib/app/home/home_controller.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'package:disha_spinning_wheel/app/data/data.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 
@@ -65,28 +66,28 @@ class HomeController extends GetxController {
 
   String? getMeaning(double degrees) {
     if (degrees > 0 && degrees < (1 / 4) * pi) {
-      return 'Is used to heal the lungs and to increase energy levels.';
+      return Data.meaningsMap['Orange'];
     }
     if (degrees > (1 / 4) * pi && degrees < (1 / 2) * pi) {
-      return 'Because brown is the color of the earth, it is often used to symbolize nature, autumn, melancholy, or death.';
+      return Data.meaningsMap['Brown'];
     }
     if (degrees > (1 / 2) * pi && degrees < (3 / 4) * pi) {
-      return 'The experts suggest that people who drive gray cars dont want to stand out and instead prefer something more subtle.';
+      return Data.meaningsMap['Gray'];
     }
     if (degrees > (3 / 4) * pi && degrees < pi) {
-      return 'Color psychology suggests that different colors can have an impact on our moods, feelings, and even behaviors. The color pink, for example, is thought to be a calming color associated with love, kindness, and femininity.';
+      return Data.meaningsMap['Pink'];
     }
     if (degrees > pi && degrees < (5 / 4) * pi) {
-      return 'Research in color psychology suggests that colors evoke psychological reactions, affecting moods and emotions.Sometimes, these reactions are related to the intensity of a color. In other cases, they are products of experience and cultural influences.';
+      return Data.meaningsMap['Green'];
     }
     if (degrees > (5 / 4) * pi && degrees < (3 / 2) * pi) {
-      return 'The color yellow can be bright and intense, which is perhaps why it can often invoke such strong feelings. Yellow can quickly grab attention, but it can also be abrasive when overused. It can appear warm and bright, yet it can also lead to visual fatigue.';
+      return Data.meaningsMap['Yellow'];
     }
     if (degrees > (3 / 2) * pi && degrees < (7 / 4) * pi) {
-      return 'As many of our readers have suggested, the color white can feel fresh and clean. The color is often used to evoke a sense of youth and modernity.';
+      return Data.meaningsMap['White'];
     }
     if (degrees > (7 / 4) * pi && degrees < 6.4) {
-      return 'Red is a bold, attention-getting color, so preferring this type of car might mean you want to project an image of power, action, and confidence.';
+      return Data.meaningsMap['Red'];
     }
     return null;
   }
